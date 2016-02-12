55e2fad4b5eb4787cc5e1b97ce25bd7579aece8c
soorajtrs <sooraj.trs@gmail.com>
2016-02-12 23:13:35 +0530

    Update AppTest.java

--- src/test/java/com/spark/liberty/spark_poc/AppTest.java
+++ src/test/java/com/spark/liberty/spark_poc/AppTest.java
@@ -14,7 +14,6 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.Set;
 import java.util.concurrent.*;
 
@@ -65,119 +64,26 @@ public class AppTest implements Serializable {
 
   @Before
   public void setUp() {
-    sc = new JavaSparkContext("local", "Junit_spark");
+   // sc = new JavaSparkContext("local", "Junit_spark");
     tempDir = Files.createTempDir();
     tempDir.deleteOnExit();
   }
 
   @After
   public void tearDown() {
-    sc.stop();
-    sc = null;
+    //sc.stop();
+    //sc = null;
   }
 
-  @SuppressWarnings("unchecked")
-  @Test
-  public void sparkContextUnion() {
-    // Union of non-specialized JavaRDDs
-    List<String> strings = Arrays.asList("Hello", "World");
-    JavaRDD<String> s1 = sc.parallelize(strings);
-    JavaRDD<String> s2 = sc.parallelize(strings);
-    // Varargs
-    JavaRDD<String> sUnion = sc.union(s1, s2);
-    Assert.assertEquals(4, sUnion.count());
-    // List
-    List<JavaRDD<String>> list = new ArrayList();
-    list.add(s2);
-    sUnion = sc.union(s1, list);
-    Assert.assertEquals(4, sUnion.count());
-
-    // Union of JavaDoubleRDDs
-    List<Double> doubles = Arrays.asList(1.0, 2.0);
-    JavaDoubleRDD d1 = sc.parallelizeDoubles(doubles);
-    JavaDoubleRDD d2 = sc.parallelizeDoubles(doubles);
-    JavaDoubleRDD dUnion = sc.union(d1, d2,d2);
-    Assert.assertEquals(6, dUnion.count());
-
-    // Union of JavaPairRDDs
-    List<Tuple2<Integer, Integer>> pairs = new ArrayList();
-    pairs.add(new Tuple2(1, 2));
-    pairs.add(new Tuple2(3, 4));
-    JavaPairRDD<Integer, Integer> p1 = sc.parallelizePairs(pairs);
-    JavaPairRDD<Integer, Integer> p2 = sc.parallelizePairs(pairs);
-    JavaPairRDD<Integer, Integer> pUnion = sc.union(p1, p2);
-    Assert.assertEquals(4, pUnion.count());
-  }
-
-  @SuppressWarnings("unchecked")
-  @Test
-  public void intersection() {
-    List<Integer> ints1 = Arrays.asList(1, 10, 2, 3, 4, 5);
-    List<Integer> ints2 = Arrays.asList(1, 6, 2, 3, 7, 8);
-    JavaRDD<Integer> s1 = sc.parallelize(ints1);
-    JavaRDD<Integer> s2 = sc.parallelize(ints2);
-
-    JavaRDD<Integer> intersections = s1.intersection(s2);
-    Assert.assertEquals(3, intersections.count());
-
-    JavaRDD<Integer> empty = sc.emptyRDD();
-    JavaRDD<Integer> emptyIntersection = empty.intersection(s2);
-    Assert.assertEquals(0, emptyIntersection.count());
-
-    List<Double> doubles = Arrays.asList(1.0, 2.0);
-    JavaDoubleRDD d1 = sc.parallelizeDoubles(doubles);
-    JavaDoubleRDD d2 = sc.parallelizeDoubles(doubles);
-    JavaDoubleRDD dIntersection = d1.intersection(d2);
-    Assert.assertEquals(2, dIntersection.count());
-
-    List<Tuple2<Integer, Integer>> pairs = new ArrayList();
-    pairs.add(new Tuple2(1, 2));
-    pairs.add(new Tuple2(3, 4));
-    JavaPairRDD<Integer, Integer> p1 = sc.parallelizePairs(pairs);
-    JavaPairRDD<Integer, Integer> p2 = sc.parallelizePairs(pairs);
-    JavaPairRDD<Integer, Integer> pIntersection = p1.intersection(p2);
-    Assert.assertEquals(2, pIntersection.count());
-  }
+  
   
   
 
   @SuppressWarnings("unchecked")
   @Test
   public void WC_test() {
-//	  List<String> strings = Arrays.asList("Hello how hello", "World ");
-	  
-	  Scanner s = null;
-	try {
-		s = new Scanner(new File("input.txt"));
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	  List<String> strings = new ArrayList<String>();
-	  while (s.hasNext()){
-		  strings.add(s.next());
-	  }
-	  s.close();
-	  
-	  
-	  JavaRDD<String> input = sc.parallelize(strings);
-	    // Split up into words.
-	    JavaRDD<String> words = input.flatMap(
-	      new FlatMapFunction<String, String>() {
-	        public Iterable<String> call(String x) {
-	          return Arrays.asList(x.split(" "));
-	        }});
-	    // Transform into word and count.
-	    JavaPairRDD<String, Integer> counts = words.mapToPair(
-	      new PairFunction<String, String, Integer>(){
-	        public Tuple2<String, Integer> call(String x){
-	          return new Tuple2(x, 1);
-	        }}).reduceByKey(new Function2<Integer, Integer, Integer>(){
-	            public Integer call(Integer x, Integer y){ return x + y;}});
-	    // Save the word count back out to a text file, causing evaluation.
-	  //  counts.saveAsTextFile(outputFile);
-	    System.out.println("output ------->" + counts.toArray());
-    Assert.assertEquals(7, counts.count());
+	App o_wc = new App();
+    Assert.assertEquals(7, o_wc.count_word("src/input.txt"));
     
   }
   
